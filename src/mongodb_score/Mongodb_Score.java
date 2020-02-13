53b58fd48614562f3035a28df157d73da6efc27d
oracle <oracle@localhost>
2020-02-13 14:04:58 +0100

    Enunciado

---
+++ src/mongodb_score/Mongodb_Score.java
@@ -0,0 +1,37 @@
+/*
+ * EJercicio de prueba de conexión y utilizacion de mongobd
+DB: TRAINIG - DOCUMENTO- SCORES
+     
+ *      1)Inserir un documento
+        2)Actualizar un par clave/valor
+        3)Incrementar un par
+        4)Consulta un docuento por su _id, _valor. DATOS INDIVIDUALES
+        5)Consulta  documentos por un par clave/valor (NO CLAVE PRIMARIA)
+        6)Consulta todos los documentos:
+            a. Por igualdad
+            b. Por mayor /menor
+            c. entre dos valores
+        7)Guardar campos de un documento por separado
+            
+ */
+
+/*
+AÑADIR LIBRERIA 
+/home/oracle/drivers/mogno-java-driver-3.4.2.jar
+*/
+package mongodb_score;
+
+/**
+ *
+ * @author Lara
+ */
+public class Mongodb_Score {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
